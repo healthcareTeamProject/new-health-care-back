6d0025c88fcd63333356f2a1afe500e82a647768
garedunord21 <garedunord21@gmail.com>
2024-11-13 17:49:36 +0900

    API수정, 게시판 코드 누락된 것 수정 후 포스트맨 테스트 완료

--- src/main/java/com/example/healthcare_back/service/CustomerService.java
+++ src/main/java/com/example/healthcare_back/service/CustomerService.java
@@ -14,12 +14,24 @@ import com.example.healthcare_back.dto.response.customer.GetUserThreeMajorLiftLi
 
 public interface CustomerService {
 
+    // 로그인 사용자 정보 조회
     ResponseEntity<? super GetSignInResponseDto> getSignIn(String userId);
+
+    // 사용자 세부 정보 조회
     ResponseEntity<? super GetCustomerResponseDto> getCustomer(String userId);
+
+    // 사용자 신체정보 조회
     ResponseEntity<? super GetUserMuscleFatListResponseDto> getUserMuscleFatList(String userId);
+
+    // 사용자 3대측정 정보 조회
     ResponseEntity<? super GetUserThreeMajorLiftListResponseDto> getUserThreeMajorLiftList(String userId);
 
+    // 사용자 정보 수정
     ResponseEntity<ResponseDto> patchCustomer(PatchCustomerRequestDto dto, String userId);
+
+    // 사용자 신체정보 수정
     ResponseEntity<ResponseDto> patchUserMuscleFatCustomer(PatchUserMuscleFatRequestDto dto, String userId);
+
+    // 사용자 3대측정 정보 수정
     ResponseEntity<ResponseDto> patchThreeMajorLiftCustomer(PatchUserThreeMajorLiftRequestDto dto, String userId);
 }
