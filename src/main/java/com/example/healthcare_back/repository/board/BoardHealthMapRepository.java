2694c40ccee086fc83884bc061d49efcda4d45ca
garedunord21 <garedunord21@gmail.com>
2024-11-20 11:37:42 +0900

    boardServiceImplement

--- src/main/java/com/example/healthcare_back/repository/board/BoardHealthMapRepository.java
+++ src/main/java/com/example/healthcare_back/repository/board/BoardHealthMapRepository.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Repository;
 
 import com.example.healthcare_back.entity.board.BoardHealthMapEntity;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface BoardHealthMapRepository extends JpaRepository<BoardHealthMapEntity, Integer> {
 
     BoardHealthMapEntity findByBoardNumber(Integer boardNumber); // 게시물 번호와 연관된 지도 데이터 조회
 
+    @Transactional
     void deleteByBoardNumber(Integer boardNumber); // 게시물 번호와 연관된 지도 데이터 삭제
 
 }
